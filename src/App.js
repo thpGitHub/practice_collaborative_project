80c41888e6db2ee5744c194ad4fdd4b1140cb6ae
Tamara Wenner <tamara-76@hotmail.fr>
2022-01-24 18:02:37 +0100

    TamComponent added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ThierryComponent from './Components/Thierry/Thierry';
+import TamComponent from './Components/Tam/Tam';
 
 function App() {
   return (
     <div className="App">
       <ThierryComponent says='Hello,'/>
+      <TamComponent says='Salut =D'/>
     </div>
   );
 }
