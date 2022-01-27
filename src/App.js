54b58a229548383869d53000690121ff314dfd7c
michelD-dev <delaunaymichel@hotmail.fr>
2022-01-27 17:19:50 +0100

    added MichelComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ThierryComponent from './Components/Thierry/Thierry';
+import MichelComponent from './Components/Michel/Michel';
 
 function App() {
   return (
     <div className="App">
       <ThierryComponent says='Hello,'/>
+      <MichelComponent says='Click me?'/>
     </div>
   );
 }
---
+++ src/Components/Michel/Michel.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from 'react'
+import './Michel.css'
+import VanillaTilt from 'vanilla-tilt'
+
+const Michel = ({ says }) => {
+  const [style, setStyle] = useState('michelComponent')
+  const [text, setText] = useState('')
+  const tiltRef = useRef()
+
+  const changeStyle = () => {
+    setStyle('michelComponent angryRed')
+    setText('Plus jamais ça!!!')
+  }
+
+  useEffect(() => {
+    const tiltNode = tiltRef.current
+    VanillaTilt.init(tiltNode, {
+      max: 25,
+      speed: 800,
+      glare: false,
+      max_glare: 0.5,
+    })
+  }, [])
+
+  return (
+    <div ref={tiltRef} className={style} onClick={changeStyle}>
+      {text || says}
+    </div>
+  )
+}
+
+export default Michel
