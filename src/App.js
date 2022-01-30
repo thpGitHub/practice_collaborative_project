4f9bfd885f97c1eb3ed77e8b01b88fa0c4b4d503
petuniahtf <priscilliaweber6@hotmail.com>
2022-01-30 23:11:19 +0100

    Suppression ligne inutil

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Petunia from './Components/Petunia/petunia';
 import Legume from './Components/Petunia/legume';
 import React from 'react';
 import SuperCastor from './Components/Petunia/superCastor';
-import Homerie from './Components/Petunia/hommerie';
+
 
 function App() {
   //const [tomateChoix, setTomateChoix] = React.useState (si tu a besoin d'utiliser tomate)
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <Homerie/>
+      
       <ThierryComponent says='Hello,'/>
       <TamComponent says='Salut =D'/>
       <NadineComponent says='Yes we can'/>
