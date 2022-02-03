5c3b8c241ba76c5513282cd7a1587c279b783b1d
danmicka <mickael.dancin@gmail.com>
2022-02-03 16:50:05 +0100

    ajout composant Mickael

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import './App.css';
-import React from 'react';
-import ThierryComponent from './Components/Thierry/Thierry';
-import TamComponent from './Components/Tam/Tam';
-import NadineComponent from './Components/Nadine/Nadine';
-import Petunia from './Components/Petunia/petunia';
-import MichelComponent from './Components/Michel/Michel';
-import DelirCompo from './Components/Petunia/delirCompo';
-
+import "./App.css";
+import React from "react";
+import ThierryComponent from "./Components/Thierry/Thierry";
+import TamComponent from "./Components/Tam/Tam";
+import NadineComponent from "./Components/Nadine/Nadine";
+import Petunia from "./Components/Petunia/petunia";
+import MichelComponent from "./Components/Michel/Michel";
+import DelirCompo from "./Components/Petunia/delirCompo";
+import MickaelComponent from "./Components/Mickael/Mickael";
 
 function App() {
-
-
-
   return (
     <div className="App">
-      <ThierryComponent says='Hello,'/>
-      <TamComponent says='Salut =D'/>
-      <NadineComponent says='Yes we can'/>
-      <MichelComponent says='Click me?'/>
-      <Petunia/>
-      <DelirCompo/>
+      <ThierryComponent says="Hello," />
+      <TamComponent says="Salut =D" />
+      <NadineComponent says="Yes we can" />
+      <MickaelComponent says="merci" />
+      <MichelComponent says="Click me?" />
+      <Petunia />
+      <DelirCompo />
     </div>
   );
 }
---
+++ src/Components/Mickael/Mickael.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Mickael.css";
+
+export default function Mickael({ says }) {
+  return <div className="mickaelComponent">{says}</div>;
+}
